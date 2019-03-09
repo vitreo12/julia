902d342b4a4f8c09c7977e5382ba7f21889c98c4
Unknown <francesco.cameli95@gmail.com>
2019-03-09 22:45:37 +0100

    Refactor code for __JuliaDef__. Now it takes only the module and the method instances

--- stdlib/JuliaCollider/src/JuliaDef.jl
+++ stdlib/JuliaCollider/src/JuliaDef.jl
@@ -4,30 +4,29 @@ module JuliaDef
     
     #Mirrored to JuliaObject in C
     struct __JuliaDef__
+        #= Module contains the functions aswell. No need to include those too =#
         evaluated_module::Base.RefValue{Module}
-        ugen_ref_fun::Base.RefValue{Function}
-        constructor_fun::Base.RefValue{Function}
-        perform_fun::Base.RefValue{Function}
-        destructor_fun::Base.RefValue{Function}
+        
+        #= MethodInstance table for this JuliaDef =#
         ugen_ref_instance::Base.RefValue{Core.MethodInstance}
         constructor_instance::Base.RefValue{Core.MethodInstance}
         perform_instance::Base.RefValue{Core.MethodInstance}
         destructor_instance::Base.RefValue{Core.MethodInstance}
+        set_index_ugen_ref_instance::Base.RefValue{Core.MethodInstance}
+        delete_index_ugen_ref_instance::Base.RefValue{Core.MethodInstance}
         
         #Should I construct it with Base.RefValue{Module}, Base.RefValue{Function} directly??
         #Module, Function and MethodInstance are just pointers, anyway...
-        function __JuliaDef__(e::Module, u_f::Function, c_f::Function, p_f::Function, d_f::Function, u_i::Core.MethodInstance, c_i::Core.MethodInstance, p_i::Core.MethodInstance, d_i::Core.MethodInstance)
-            e_r::Base.RefValue{Module}     = Base.RefValue{Module}(e)
-            u_f_r::Base.RefValue{Function} = Base.RefValue{Function}(u_f)
-            c_f_r::Base.RefValue{Function} = Base.RefValue{Function}(c_f)
-            p_f_r::Base.RefValue{Function} = Base.RefValue{Function}(p_f)
-            d_f_r::Base.RefValue{Function} = Base.RefValue{Function}(d_f)
-            u_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(u_i)
-            c_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(c_i)
-            p_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(p_i)
-            d_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(d_i)
-            
-            return new(e_r, u_f_r, c_f_r, p_f_r, d_f_r, u_i_r, c_i_r, p_i_r, d_i_r)
+        function __JuliaDef__(e::Module, u_i::Core.MethodInstance, c_i::Core.MethodInstance, p_i::Core.MethodInstance, d_i::Core.MethodInstance, s_i_i::Core.MethodInstance, d_i_i::Core.MethodInstance)
+            e_r::Base.RefValue{Module}                  = Base.RefValue{Module}(e)
+            u_i_r::Base.RefValue{Core.MethodInstance}   = Base.RefValue{Core.MethodInstance}(u_i)
+            c_i_r::Base.RefValue{Core.MethodInstance}   = Base.RefValue{Core.MethodInstance}(c_i)
+            p_i_r::Base.RefValue{Core.MethodInstance}   = Base.RefValue{Core.MethodInstance}(p_i)
+            d_i_r::Base.RefValue{Core.MethodInstance}   = Base.RefValue{Core.MethodInstance}(d_i)
+            s_i_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(s_i_i)
+            d_i_i_r::Base.RefValue{Core.MethodInstance} = Base.RefValue{Core.MethodInstance}(d_i_i)
+
+            return new(e_r, u_i_r, c_i_r, p_i_r, d_i_r, s_i_i_r, d_i_i_r)
         end
     end
 end
